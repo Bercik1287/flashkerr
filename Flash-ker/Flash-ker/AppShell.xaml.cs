58504f3c29a34dba6c0016682b870c3293e03238
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-10-28 22:25:45 +0100

    Aplikacja xamarine część 2

---
+++ Flash-ker/Flash-ker/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Flash_ker.ViewModels;
+using Flash_ker.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Flash_ker
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Flash-ker/Flash-ker/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Flash_ker.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "Flaskher";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
